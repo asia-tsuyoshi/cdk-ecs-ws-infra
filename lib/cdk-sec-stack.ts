ac7befca27a4111720df7f9f0e83f6e379bb0902
S1045 <s1045@S1045.local>
2023-11-20 23:06:30 +0900

    first

--- lib/cdk-sec-stack.ts
+++ lib/cdk-sec-stack.ts
@@ -1,16 +1,64 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
 
 export class CdkSecStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) 
+  {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "vpc", {
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+      subnetConfiguration: [
+        {
+          name: "isolated",
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED
+        }
+      ]
+    })
+    // ECS用
+    vpc.addInterfaceEndpoint("ecr-endpoint", {
+      service: ec2.InterfaceVpcEndpointAwsService.ECR
+    })
+    vpc.addInterfaceEndpoint("ecr-dkr-endpoint", {
+      service: ec2.InterfaceVpcEndpointAwsService.ECR_DOCKER
+    })
+    vpc.addInterfaceEndpoint("logs-endpoint", {
+      service: ec2.InterfaceVpcEndpointAwsService.CLOUDWATCH_LOGS
+    })
+    vpc.addGatewayEndpoint("s3-endpoint", {
+      service: ec2.GatewayVpcEndpointAwsService.S3,
+      subnets: [
+        {
+          subnets: vpc.isolatedSubnets
+        }
+      ]
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkSecQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
+    const repo = ecr.Repository.fromRepositoryName(this, "repo", "watanabe-new")
+
+    const cluster = new ecs.Cluster(this, "cluster", { vpc })
+    const taskDefinition = new ecs.TaskDefinition(this, "taskdef", {
+      compatibility: ecs.Compatibility.FARGATE,
+      cpu: "256",
+      memoryMiB: "512",
+    })
+    taskDefinition.addContainer("laravel", {
+      // docker側できたら差し替える
+      image: ecs.ContainerImage.fromEcrRepository(repo),
+      // image: ecs.ContainerImage.fromRegisstry("/amazon-ecs-sample"),
+      logging: ecs.LogDriver.awsLogs({ streamPrefix: 'test-on-fargate' }),
+      // プロトコルtcp 9000で設定
+      portMappings: [{containerPort: 9000, protocol: ecs.Protocol.TCP}],
+      // readonlyRootFilesystem: false
+    })
+    new ecs.FargateService(this, "service", {
+      cluster,
+      taskDefinition,
+      enableExecuteCommand: true,
+    })
   }
 }
